c5814b7f1bfc6d6807414b114747e93d58ceb7bd
정성태 <59467639+jst0951@users.noreply.github.com>
2023-10-08 20:43:50 +0900

    [level 2] Title: 방문 길이, Time: 4.37 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
@@ -0,0 +1,30 @@
+def solution(direction_list):
+    history_list = []
+    position = [0, 0]
+    count = 0
+    for direction in direction_list:
+        if direction == 'U':
+            diff = [0, 1]
+        elif direction == 'D':
+            diff = [0, -1]
+        elif direction == 'R':
+            diff = [1, 0]
+        elif direction == 'L':
+            diff = [-1, 0]
+        if position[0] + diff[0] < -5 or position[0] + diff[0] > 5 or position[1] + diff[1] < -5 or position[1] + diff[1] > 5:
+            continue
+        new_position = [position[0] + diff[0], position[1] + diff[1]]
+        
+        # 카운트
+        isNew = True
+        for history in history_list:
+            # 순서가 동일한 경우
+            if (history[0] == position[0] and history[1] == position[1] and history[2] == new_position[0] and history[3] == new_position[1]) or (history[0] == new_position[0] and history[1] == new_position[1] and history[2] == position[0] and history[3] == position[1]):
+                isNew = False
+                break
+        if isNew == True:
+            count += 1
+            history_list.append(position + new_position)
+            
+        position = new_position
+    return count
